ba482affac52fe286dff67a8a81381d9b272ec32
Egidijus <e.grajauskas@gmail.com>
2022-07-07 20:23:48 +0300

    Create Darbuotojas class intoo root

---
+++ src/main/java/lt/egidijusman/Darbuotojas.java
@@ -0,0 +1,76 @@
+package lt.egidijusman;
+
+import java.time.LocalDate;
+
+public class Darbuotojas {
+    private String name;
+    private String lastName;
+    private String asmensKodas;
+    private int amzius;
+    private String pareigos;
+    private int stazas;
+
+    public Darbuotojas(String name, String lastName, String asmensKodas) {
+        this.name = name;
+        this.lastName = lastName;
+        this.asmensKodas = asmensKodas;
+        this.amzius = paskaiciuokAmziu();
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAsmensKodas() {
+        return asmensKodas;
+    }
+
+    public int getAmzius() {
+        return amzius;
+    }
+
+    public String getPareigos() {
+        return pareigos;
+    }
+
+    public int getStazas() {
+        return stazas;
+    }
+
+    public void setPareigos(String pareigos) {
+        this.pareigos = pareigos;
+    }
+
+    public void setStazas(int stazas) {
+        this.stazas = stazas;
+    }
+
+    private int paskaiciuokAmziu() {
+
+        char pirmasAkSkaicius = asmensKodas.charAt(0);
+        int gimimoMetai = Integer.parseInt(asmensKodas.substring(1, 3));
+        int einamiMetai = LocalDate.now().getYear();
+
+        switch (pirmasAkSkaicius) {
+            case '3':
+            case '4':
+                gimimoMetai += 1900;
+                break;
+            case '5':
+            case '6':
+                gimimoMetai += 2000;
+                break;
+            default:
+                System.out.println("Ivestas blogai asmens kodas");
+
+
+        }
+        return einamiMetai - gimimoMetai;
+    }
+}
---
+++ src/main/java/lt/egidijusman/Main.java
@@ -0,0 +1,22 @@
+package lt.egidijusman;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        char kintChar = 'A';
+//        int kintInt = kintChar;
+//
+//        System.out.println(kintInt);
+//        System.out.println(kintChar);
+
+        Darbuotojas darbuotojas1 = new Darbuotojas( "Charlie", "Chaplin", "3020410123");
+        Darbuotojas darbuotojas2 = new Darbuotojas("tomas", "jeris", "5020410123");
+
+        System.out.println(darbuotojas1);
+
+
+
+
+
+    }
+}
